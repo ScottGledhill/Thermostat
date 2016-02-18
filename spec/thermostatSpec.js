716e50b2dea5d56d0e3455ede04c59277cf0437c
JoeWroe <joe_wroe@icloud.com>
2016-02-18 10:32:51 +0000

    USER STORY ONE
    Added jasmine files and setup
    Completed first user story
    
    	Thermostat starts at 20 degrees

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,11 @@
+describe('Thermostat', function() {
+
+  var thermostat = new Thermostat();
+
+  describe('temperature', function() {
+
+    it('starts at 20 degrees', function() {
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+});
