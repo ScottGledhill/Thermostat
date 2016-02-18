548fbc41dab7e0e2d761a0bfcfb15def41ecd27c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-02-18 10:49:41 +0000

    upButton added and tested

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,5 +7,10 @@ describe('Thermostat', function() {
     it('starts at 20 degrees', function() {
       expect(thermostat.temperature).toEqual(20);
     });
+
+    it('Can increase temperature with upbutton', function() {
+      thermostat.upButton();
+      expect(thermostat.temperature).toEqual(21);
+    });
   });
 });
